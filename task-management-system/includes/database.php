4ace2a209db29a0cde3a3505ec52acb37dff0bbb
johnbedeir <john.bedeir@gmail.com>
2024-07-14 01:16:21 +0200

    skip ci

--- task-management-system/includes/database.php
+++ task-management-system/includes/database.php
@@ -1,5 +1,7 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
+
+use Dotenv\Dotenv;
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->load();
